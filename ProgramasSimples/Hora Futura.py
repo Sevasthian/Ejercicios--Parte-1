04457b792b82f1981e635f24ad26fbc284dbcc02
Sevasthian <160695272+Sevasthian@users.noreply.github.com>
2024-03-13 13:03:43 -0500

    arreglar

---
+++ ProgramasSimples/Hora Futura.py
@@ -0,0 +1,3 @@
+horaActual = int(input("Escriba la hora en que estas (ejem: son las 4:50 usted escribe que son las 4):"))
+CantidadDeHoras = int(input("Escriba una cantidad de horas: "))
+if horaActual >=
